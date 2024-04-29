15588917dc9f25891ffbfb54ff04caceaa7e3eeb
Rudra Pratap Singh <144105327+Rudra78996@users.noreply.github.com>
2024-04-29 09:18:02 +0530

    updated bd

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
     password:  process.env.DB_PASSWORD,
-    database: 'employee',
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit:10,
+    queueLimit:0
 });
 module.exports = connection;
