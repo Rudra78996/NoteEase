94089f6048f1769e2aeda91fc8151ca04b60cc9c
Rudra Pratap Singh <144105327+Rudra78996@users.noreply.github.com>
2024-04-29 10:02:19 +0530

    change  env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT ;
 const cookieParser = require("cookie-parser");
 const cookieJwtAuth = require('./middleware/cookieJwtAuth');
 const path = require("path");
